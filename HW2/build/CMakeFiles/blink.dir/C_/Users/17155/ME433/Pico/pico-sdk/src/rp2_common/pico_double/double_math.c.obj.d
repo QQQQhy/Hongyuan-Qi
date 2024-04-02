7aa26ed3ca79238c33f0881fe670ad390cf1ba8c
QQQQhy <qhy20001102@gmail.com>
2024-04-02 16:28:50 -0500

    adding files

---
+++ HW2/build/CMakeFiles/blink.dir/C_/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_double/double_math.c.obj.d
@@ -0,0 +1,39 @@
+CMakeFiles/blink.dir/C_/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_double/double_math.c.obj: \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_double/double_math.c \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\math.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\reent.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\newlib.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\config.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\lock.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\cdefs.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_double/include/pico/double.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\assert.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ C:/Users/17155/ME433/Hongyuan-Qi/HW2/build/generated/pico_base/pico/version.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ C:/Users/17155/ME433/Hongyuan-Qi/HW2/build/generated/pico_base/pico/config_autogen.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/boards/include/boards/pico.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom/sf_table.h
