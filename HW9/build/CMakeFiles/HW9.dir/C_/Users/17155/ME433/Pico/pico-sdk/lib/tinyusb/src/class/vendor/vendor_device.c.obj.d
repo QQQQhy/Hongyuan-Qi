30b5be03f2613ebbff795e6b0bddc43e13f6ef23
QQQQhy <qhy20001102@gmail.com>
2024-06-01 16:29:31 -0500

    adding files

---
+++ HW9/build/CMakeFiles/HW9.dir/C_/Users/17155/ME433/Pico/pico-sdk/lib/tinyusb/src/class/vendor/vendor_device.c.obj.d
@@ -0,0 +1,37 @@
+CMakeFiles/HW9.dir/C_/Users/17155/ME433/Pico/pico-sdk/lib/tinyusb/src/class/vendor/vendor_device.c.obj: \
+ C:/Users/17155/ME433/Pico/pico-sdk/lib/tinyusb/src/class/vendor/vendor_device.c \
+ C:/Users/17155/ME433/Pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/tusb_config.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\assert.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\newlib.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\config.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/generated/pico_base/pico/version.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/generated/pico_base/pico/config_autogen.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/boards/include/boards/pico.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\cdefs.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ C:/Users/17155/ME433/Pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
---
+++ HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/tmp/PioasmBuild-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/17155/ME433/Pico/pico-sdk/tools/pioasm"
+  "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pioasm"
+  "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm"
+  "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/tmp"
+  "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/src/PioasmBuild-stamp"
+  "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/src"
+  "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/src/PioasmBuild-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/src/PioasmBuild-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/17155/ME433/Hongyuan-Qi/HW9/build/pico-sdk/src/rp2_common/tinyusb/pioasm/src/PioasmBuild-stamp${cfgdir}") # cfgdir has leading slash
+endif()
